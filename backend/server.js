f804152d432078391c24d92ca86919664aac1754
PharesMwangi <mwasmwas683@gmail.com>
2026-06-22 17:28:17 +0300

    changed port

--- backend/server.js
+++ backend/server.js
@@ -24,5 +24,5 @@ app.use((err, req, res, next) =>{
 
 const PORT = process.env.port || 5000;
 app.listen(PORT, ()=>{
-    console.log(`Server running on http://localhost:$PORT`);
+    console.log(`Server running on http://localhost:${PORT}`);
 });
