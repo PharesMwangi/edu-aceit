875468454f3ca604a166dd32608a89f3605e0c0b
PharesMwangi <mwasmwas683@gmail.com>
2026-07-01 18:10:41 +0300

    changed on how to consume APIs

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,5 +1,5 @@
 //to call endpoints
-const API_BASE = import.meta.env.VITE_BACKEND_URL || 'http://localhost:5000/api';
+const API_BASE = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/api';
 
 async function request(path, options = {}) {
     const res = await fetch(`${API_BASE}${path}`, {
